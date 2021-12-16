f1df7c5defb01e5a627e3b86aeea68191b9cc939
vlaskinmac <kangelina86@mail.ru>
2021-12-17 00:27:13 +0300

    Fixed machine of states

--- santa_bot.py
+++ santa_bot.py
@@ -7,7 +7,6 @@ import time
 import json
 import random
 
-
 from datetime import date, timedelta
 
 from aiogram import types
@@ -17,6 +16,7 @@ from aiogram.types import KeyboardButton
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text
 from aiogram.types.message import ContentType
 from aiogram.utils.exceptions import TelegramAPIError
 import aiogram.utils.markdown as fmt
@@ -25,7 +25,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv("TG_BOT_TOKEN")
-bot_name = os.getenv("TG_BOT_NAME")
 loop = asyncio.get_event_loop()
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -33,9 +32,21 @@ dp = Dispatcher(bot, storage=storage, loop=loop)
 game_data = {}
 
 
+class RegisterOrder(StatesGroup):
+    game_id = State()
+    user_name = State()
+    user_email = State()
+    user_wishlist = State()
+    letter_to_santa = State()
+
+
 @dp.message_handler(commands='start')
-async def cmd_start(message: types.Message):
-    if not message.text == '/start reg':
+@dp.message_handler(text='Отмена')
+@dp.message_handler(Text(equals="Отмена"), state="*")
+async def cmd_start(message: types.Message, state: FSMContext):
+    if message.text == 'Отмена':
+        await state.finish()
+    if not message.text == '/start reg' or message.text == 'Отмена':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(KeyboardButton('Создать игру'))
         await bot.delete_message(message.from_user.id, message.message_id)
@@ -55,13 +66,6 @@ async def cmd_start(message: types.Message):
                 fmt.text(f"\nДата отправки подарков:   {game_data['date_send']}\n")
             ), reply_markup=keyboard
 
-                    fmt.text(
-                        fmt.text("Замечательно!\n\nТы собираешься участвовать в игре:\n\n"),
-                        fmt.text(f"Название игры:   {game_data['name_game'].upper()}\n"),
-                        fmt.text(f"\nЦеновой диапазон подарка:   {game_data['limit_price']}\n"),
-                        fmt.text(f"\nПериод регистрации участников:   {game_data['date_reg']}\n"),
-                        fmt.text(f"\nДата отправки подарков:   {game_data['date_send']}\n")
-                    ), reply_markup=keyboard
         )
 
 
@@ -83,7 +87,7 @@ async def yes_limit(call: types.CallbackQuery):
         types.InlineKeyboardButton(text='500-1000 р', callback_data='500-1000 p'),
         types.InlineKeyboardButton(text='500-2000 р', callback_data='500-2000 p'),
     ]
-    keyboard.row(*buttons)-
+    keyboard.row(*buttons)
     await bot.delete_message(call.from_user.id, call.message.message_id)
     await call.message.answer("Выберите ценовой диапазон:", reply_markup=keyboard)
     await call.answer()
@@ -132,36 +136,18 @@ async def date_send(call: types.CallbackQuery):
 async def logging_user(call: types.CallbackQuery):
     choice_day = re.search(r'\d+', call.data).group()
     date_today = datetime.date.today()
+    bot_name = await bot.get_me()
     game_data['date_send'] = f'{choice_day}.{date_today.month}.{date_today.year}'
     await bot.delete_message(call.from_user.id, call.message.message_id)
     await call.message.answer("Отлично! Тайный Санта уже готовится к раздаче подарков!",
                               reply_markup=types.ReplyKeyboardRemove())
-
     await call.message.answer(
         fmt.text(
             fmt.text("Перешлите ссылку новому участнику игры для регистрации:\n\n"),
-
-            fmt.text(f'https://t.me/{bot_name}?start=reg'),
-
-            fmt.text('https://t.me/santa_qwerty_rty_bot?start=reg'),
-
+            fmt.text(f'https://t.me/{bot_name.username}?start=reg'),
         )
     )
 
-# logic for wish sheets
-# @dp.callback_query_handler(text_contains='wish')
-# async def wish_sheet(call: types.CallbackQuery):
-#     for wish in data["wish"]:
-#         await call.message.answer(
-#             fmt.text(
-#                 fmt.text(wish["user"]),
-#                 fmt.text(wish["user"]),
-#                 fmt.text(wish["user"]),
-#             ), reply_markup=types.ReplyKeyboardRemove()
-#         )
-
-
-# nikita's blog..
 
 def validate_email(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
@@ -185,23 +171,18 @@ def add_user(user):
         print(users_db)
 
 
-class RegisterOrder(StatesGroup):
-    game_id = State()
-    user_name = State()
-    user_email = State()
-    user_wishlist = State()
-    letter_to_santa = State()
-
-
-@dp.message_handler(text=['Регистрация'])
+@dp.message_handler(Text(equals="Изменить имя"), state="*")
+@dp.message_handler(text='Регистрация')
 async def cmd_register(message: types.Message, state: FSMContext):
     try:
         game_id = game_data['game_id']
         user_id = message['from']['id']
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(KeyboardButton(text='Отмена'))
         await state.update_data(game_id=game_id)
         await state.update_data(user_id=user_id)
         await RegisterOrder.user_name.set()
-        await message.answer('Теперь укажите имя пользователя:')
+        await message.answer('Теперь укажите имя:', reply_markup=keyboard)
     except IndexError:
         await message.reply('Введите id игры.')
         await RegisterOrder.game_id.set()
@@ -216,62 +197,77 @@ async def get_game_id(message: types.Message, state: FSMContext):
         game_id = int(message.text)
         await state.update_data(game_id=game_id)
         await RegisterOrder.next()
-        await message.answer('Теперь укажите имя пользователя:')
     except ValueError:
         await message.answer('id игры должен быть целым числом')
         return
 
 
+@dp.message_handler(Text(equals="Изменить email"), state="*")
 @dp.message_handler(state=RegisterOrder.user_name)
 async def get_user_name(message: types.Message, state: FSMContext):
     user_name = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton(text='Изменить имя'), KeyboardButton(text='Отмена'))
     await state.update_data(user_name=user_name)
     await RegisterOrder.next()
-    await message.answer('Теперь укажите email пользователя:')
+    await message.answer('Теперь укажите email:', reply_markup=keyboard)
 
 
+@dp.message_handler(Text(equals="Изменить список желаний"), state="*")
 @dp.message_handler(state=RegisterOrder.user_email)
 async def get_user_email(message: types.Message, state: FSMContext):
     user_email = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton(text='Изменить email'), KeyboardButton(text='Отмена'))
     if not validate_email(user_email.strip()):
         await message.answer('Введите корректный email')
         return
     await state.update_data(user_email=user_email)
     await RegisterOrder.next()
-    await message.answer('Теперь укажите ваш вишлист (введите стоп, что бы продолжить дальше):')
+    await message.answer(
+        'Теперь укажите ваш список желаний (введите стоп, что бы продолжить дальше):', reply_markup=keyboard)
 
 
+@dp.message_handler(Text(equals="Изменить письмо санте"), state="*")
 @dp.message_handler(state=RegisterOrder.user_wishlist)
 async def get_user_wishlist(message: types.Message, state: FSMContext):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton(text='Изменить список желаний'), KeyboardButton(text='Отмена'))
     user_wishlist = message.text
     await state.update_data(user_wishlist=user_wishlist)
     await RegisterOrder.next()
-    await message.answer('Напишите письмо санте:')
+    await message.answer('Напишите письмо санте:', reply_markup=keyboard)
 
 
 @dp.message_handler(state=RegisterOrder.letter_to_santa)
 async def write_letter_to_santa(message: types.Message, state: FSMContext):
     letter = message.text
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton(text='Отправить письмо санте!'), KeyboardButton(text='Отмена'))
     await state.update_data(letter_to_santa=letter)
     user_data = await state.get_data()
     add_user(user_data)
     await state.finish()
-    await message.answer('Вы зарегистрированы на игру. Ожидайте сообщения о начале игры.')
-
-@dp.message_handler(text='Регистрация')
-async def logger(message: types.Message):
-    await message.answer(f"{message.chat.id, message.from_user.id, message.from_user.first_name, game_data['game_id']}")
-    await message.answer("Введите имя: ", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer('🎅', reply_markup=keyboard)
 
 
-
-
-
-
+@dp.message_handler(text='Отправить письмо санте!')
+async def wish_sheet(message: types.Message):
+    await message.answer('Вы зарегистрированы на игру. Ожидайте сообщения о начале игры!')
+    # for wish in data["wish"]:
+    #     await call.message.answer(
+    #         fmt.text(
+    #             fmt.text(wish["user"]),
+    #             fmt.text(wish["user"]),
+    #             fmt.text(wish["user"]),
+    #         ), reply_markup=types.ReplyKeyboardRemove()
+    #     )
 
 
 
 # !! it`s final handler____________________________________________________________
+
+
 @dp.message_handler()
 async def name_game(message: types.Message):
     game_data['name_game'] = message.text
@@ -284,6 +280,9 @@ async def name_game(message: types.Message):
     keyboard.add(button_yes, button_no)
     await message.answer(f"Для игры - {game_data['name_game'].upper()}\n\nТребуется ограничение стоимости подарка?",
                          reply_markup=keyboard)
+
+
+
 
 
 if __name__ == '__main__':
